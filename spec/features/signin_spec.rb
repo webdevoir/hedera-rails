1881f5f0f2171713b70d4790643817a888ff96d4
acandael <anthony.candaele@ugent.be>
2016-10-18 11:31:07 +0200

    implement feature spec for user signin in

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "signin in" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  def fill_in_signin_fields
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
+    click_button "Log in"
+  end
+
+  scenario "visiting the site to sign in" do
+    visit new_user_session_path
+    fill_in_signin_fields
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
