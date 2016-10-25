2f2bdb890b7f7ccf3195a8cc3deef0cf90241530
acandael <anthony.candaele@ugent.be>
2016-10-25 15:46:01 +0200

    implement guest views member details

--- spec/features/guest_views_members_spec.rb
+++ spec/features/guest_views_members_spec.rb
@@ -9,4 +9,16 @@ feature "members" do
     expect(page).to have_content member.title
     expect(page).to have_css("img[src*='member_pic']")
   end
+
+  scenario "guest views member details" do
+    visit members_path
+    click_link member.name
+    expect(current_path).to eq member_path(member)
+    expect(page).to have_css "header.pageheader h1", text: member.name
+    expect(page).to have_css "section.bio p", text: member.bio
+    expect(page).to have_link member.bibliography
+    expect(page).to have_css "section.contact p", text: member.email
+    expect(page).to have_css "section.contact p", text: member.phone
+    expect(page).to have_css "section.contact p", text: member.address
+  end
 end
