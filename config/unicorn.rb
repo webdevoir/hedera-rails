06386fc2cf9bcdec90fc084f70118269a5344e5e
acandael <anthony.candaele@ugent.be>
2016-11-14 09:17:08 +0100

    add unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+
+t = "/opt/www/hedera/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.hedera.sock"
+worker_processes 1
+timeout 30
