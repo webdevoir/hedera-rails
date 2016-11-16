701e7f3ebcc0864267e48afa26e1bac1357654c0
acandael <anthony.candaele@ugent.be>
2016-11-16 14:01:41 +0100

    edit paperclip config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     bucket: ENV['S3_BUCKET_NAME'],
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+    s3_host_name: "s3-eu-west-1.amazonaws.com",
+    s3_region: 'eu-west-1'
   }
 end
