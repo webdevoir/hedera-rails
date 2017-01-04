6469bfbd7cf307d9fa417536ee07b8f8153d8a68
acandael <anthony.candaele@ugent.be>
2017-01-04 11:38:41 +0100

    customize login template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
   root "home#index"
   get 'sortedpublications/:category', to: 'publications#sort', as: 'sortedpublications'
 
