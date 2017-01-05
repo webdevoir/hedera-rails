2cf62cd5df989a8b9d59ea5cae694da51f71464e
acandael <anthony.candaele@ugent.be>
2017-01-05 09:38:09 +0100

    implement custom 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   root "home#index"
